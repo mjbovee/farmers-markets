b32ce838924bfb5160bd0787f0bf2d9cbf8963a6
Michael Bovee <michael.j.bovee@gmail.com>
2019-03-08 12:08:54 -0700

    got projection and choropleth working

--- main.js
+++ main.js
@@ -1,18 +1,62 @@
 var width = document.querySelector('.content').offsetWidth
 var height = document.querySelector('.content').offsetHeight
 
-var div = d3.select('.content').append('div')
-    .attr('class', 'tooltip')
-    .style('opacity', 0)
+// var tip = d3.tip()
+//     .attr('class', 'd3-tip')
+//     .offset([-10, 0])
+//     .html(function(d) {
+//         return ''
+//     })
 
-var svg = d3.select('.content').append('svg')
-    .attr('width', width)
-    .attr('height', height)
+var color = d3.scaleLinear()
+    .domain([0, 1])
+    .range(['#9bf4cd', '#1f5569'])
 
-var projection = d3.geoMercator()
+var projection = d3.geoAlbersUsa()
     .translate([width / 2, height / 2])
+
 
 var path = d3.geoPath()
     .projection(projection)
 
-svg.call(tip)
+var svg = d3.select('.content').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('class', 'map')
+
+// svg.call(tip)
+
+queue()
+    .defer(d3.json, 'counties.json')
+    .defer(d3.tsv, 'marketData.csv')
+    .await(ready)
+
+function ready(error, data, farmersMarkets) {
+    var farmersMarketsById = {}
+
+    farmersMarkets.forEach(function(d) {
+        farmersMarketsById[d.GEOID] = +d.FMRKTPTH16
+    })
+    data.features.forEach(function(d) {
+        d.farmersMarkets = farmersMarketsById[d.properties.GEOID]
+    })
+
+    svg.append('g')
+        .attr('class', 'counties')
+        .selectAll('path')
+        .data(data.features)
+        .enter().append('path')
+        .attr('d', path)
+        .style('fill', function(d) {
+            return color(d.farmersMarkets)
+        })
+
+    svg.append('path')
+        .datum(topojson.mesh(data.features, function(a, b) {
+            return a.GEOID !== b.GEOID
+        }))
+        .attr('class', 'names')
+        .attr('d', path)
+}
+
